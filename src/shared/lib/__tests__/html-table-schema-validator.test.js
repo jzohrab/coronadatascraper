45a8c1e40be8f647420a8344afc2b15554a38e4b
Jeff Zohrab <jzohrab@gmail.com>
2020-03-28 10:30:29 -0600

    wip sorting out stuff

--- src/shared/lib/__tests__/html-table-schema-validator.test.js
+++ src/shared/lib/__tests__/html-table-schema-validator.test.js
@@ -1,17 +1,40 @@
 // Test
+import cheerio from 'cheerio';
 
 describe('html-table-schema-validator', () => {
   describe('success', () => {
-    test('adds 1 + 2 to equal 3', () => {
-      expect(1 + 4).toBe(3);
+    // test('adds 1 + 2 to equal 3', () => {
+    //  expect(1 + 4).toBe(3);
+    // });
+
+    test('success if no validation rules', () => {
+      const h = '<html><body><table id="tid"><tr><td>a</td></tr><tr><td>1</td></tr></table></body></html>';
+
+      const $ = cheerio.load(h);
+      console.log($.html());
+      console.log(
+        $('table#tid')
+          .first()
+          .html()
+      );
+
+      const $table = $('table#tid').eq(0);
+      const $trs = $table.find('tr');
+      console.log(`got ${$trs.length} trs`);
+      $trs.each((index, tr) => {
+        const $tr = $(tr);
+        console.log($tr.find('td:first-child').text());
+      });
     });
   });
 
   /*
   describe('errors', () => {
-    test('adds 1 + 2 to equal 3', () => {
-      expect(1 + 3).toBe(3);
-    });
+null table
+rule failed
+empty table
+... try each spec rule in order
+etc
   });
 
   describe('checks invalid schema', () => {
