6aeb443e6b3a2657ffd945cd359240d06a85c5ca
Jeff Zohrab <jzohrab@gmail.com>
2020-03-28 10:53:22 -0600

    wip

--- src/shared/lib/__tests__/html-table-schema-validator.test.js
+++ src/shared/lib/__tests__/html-table-schema-validator.test.js
@@ -1,30 +1,51 @@
 // Test
 import cheerio from 'cheerio';
 
+// TODO: move this to its own file
+// TODO: code review on ... code style and everything, naming conventions, etc.
+class HtmlTableValidor {
+  constructor(rules) {
+    this.rules = rules;
+  }
+
+  /* eslint-disable class-methods-use-this, no-unused-vars */
+  success(table) {
+    // hacks to get past lint.
+    table = null;
+    return true;
+  }
+  /* eslint-ensable class-methods-use-this, no-unused-vars */
+}
+
 describe('html-table-schema-validator', () => {
   describe('success', () => {
-    // test('adds 1 + 2 to equal 3', () => {
-    //  expect(1 + 4).toBe(3);
-    // });
-
     test('success if no validation rules', () => {
       const h = '<html><body><table id="tid"><tr><td>a</td></tr><tr><td>1</td></tr></table></body></html>';
 
       const $ = cheerio.load(h);
+      /*
       console.log($.html());
       console.log(
         $('table#tid')
           .first()
           .html()
       );
+      */
 
       const $table = $('table#tid').eq(0);
+
+      /*
       const $trs = $table.find('tr');
       console.log(`got ${$trs.length} trs`);
       $trs.each((index, tr) => {
         const $tr = $(tr);
         console.log($tr.find('td:first-child').text());
       });
+      */
+
+      const rules = {};
+      const v = new HtmlTableValidor(rules);
+      expect(v.success($table)).toBe(true);
     });
   });
 
