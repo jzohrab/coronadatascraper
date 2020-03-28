638ba56b0807c12aaaa70c734f21d25f885e9c9c
Jeff Zohrab <jzohrab@gmail.com>
2020-03-28 16:58:14 -0600

    wip tests passing

--- src/shared/lib/__tests__/html-table-schema-validator.test.js
+++ src/shared/lib/__tests__/html-table-schema-validator.test.js
@@ -344,7 +344,10 @@ describe('html-table-schema-validator', () => {
           split(/[\r\n]+/).
           filter(lin => lin.trim() != '').
           map(lin => lin.replace(/^ +/g, '')).
-          map(lin => lin.split('|').map(el => '<td>' + el.trim() + '</td>').join(''));
+          map(lin => lin.split('|').
+              map(el => `<td>${el.trim()}</td>`).join('')
+             ).
+          map(lin => `<tr>${lin.trim()}</tr>`).join("\n");
       // let rows = lines.
          //  map(lin 
           // map(lin => `<tr><td>${lin.join('</td>
