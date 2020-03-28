ddf7a3f7bdece5620f1319c74191c6df6f72a897
Jeff Zohrab <jzohrab@gmail.com>
2020-03-28 09:14:40 -0600

    wip

---
+++ src/shared/lib/__tests__/html-table-schema-validator.test.js
@@ -0,0 +1,23 @@
+// Test
+
+describe('html-table-schema-validator', () => {
+  describe('success', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(1 + 4).toBe(3);
+    });
+  });
+
+  /*
+  describe('errors', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(1 + 3).toBe(3);
+    });
+  });
+
+  describe('checks invalid schema', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(1 + 4).toBe(3);
+    });
+  });
+  */
+});
