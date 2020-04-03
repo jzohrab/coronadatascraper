74979b869e0bcc1c86bbe382a6885e8cb8c874ba
Jeff Zohrab <jzohrab@gmail.com>
2020-04-03 09:34:17 -0600

    cleanup PASSING

--- tests/integration/scrapers/scrapers-all-test.js
+++ tests/integration/scrapers/scrapers-all-test.js
@@ -61,12 +61,10 @@ class Lock {
 // docs/sources.md#testing-sources
 
 
-const cachePath = join(process.cwd(), 'tests', 'integration', 'scrapers', 'testcache');
-
 // Splits folder path, returns hash.
 // e.g. 'X/Y/2020-03-04' => { scraperName: 'X/Y', date: '2020-03-04' }
 function scraperNameAndDateFromPath(s) {
-  const parts = s.replace(`${cachePath}${path.sep}`, '').split(path.sep);
+  const parts = s.split(path.sep);
 
   const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
   const scraper_name = parts.filter(s => !dateRegex.test(s)).join(path.sep);
@@ -77,52 +75,50 @@ function scraperNameAndDateFromPath(s) {
   return ret;
 }
 
-async function runTest(t, testDirectory) {
+async function runTest(t, cacheRoot, testDirectory) {
   const pair = scraperNameAndDateFromPath(testDirectory);
   const sname = pair.scraperName;
   const date = pair.date;
 
-  const scraperSourcePathRoot = join(__dirname, '..', '..', '..', 'src', 'shared', 'scrapers');
-  const spath = join(scraperSourcePathRoot, sname, 'index.js');
-
   get.get = async (url, type, date, options) => {
     const sanurl = sanitize.sanitizeUrl(url);
     const respFile = join(testDirectory, sanurl);
-    console.log(`  Call: ${url}\n  Sanitized: ${sanurl}\n  Response: ${respFile}`);
-    /*
-      console.log("CALLING FOR " + url);
-      console.log("SANITIZED: " + sanurl);
-      console.log("RESPONSE FILE: " + respFile);
-    */
-    return await fs.readFile(respFile);
+    // console.log(`  Call: ${url}\n  Sanitized: ${sanurl}\n  Response: ${respFile}`);
+    return await fs.readFile(join(cacheRoot, respFile));
   };
 
-  const fullExpected = await fs.readJSON(join(testDirectory, 'expected.json'));
-  
+  const scraperSourcePathRoot = join(__dirname, '..', '..', '..', 'src', 'shared', 'scrapers');
+  const spath = join(scraperSourcePathRoot, sname, 'index.js');
   const scraperObj = imports(spath).default;
-  process.env.SCRAPE_DATE = date;
 
   let result = null;
   try {
+    process.env.SCRAPE_DATE = date;
     result = await runScraper.runScraper(scraperObj);
   }
   catch (e) {
     t.fail(`${sname} on ${date}, error scraping: ${e}`);
   }
-
-  delete process.env.SCRAPE_DATE;
+  finally {
+    delete process.env.SCRAPE_DATE;
+  }
 
   if (result) {
     // Writing the actual scraper result so ppl can diff/investigate.
     // These are ignored in .gitignore.
-    await fs.writeJSON(join(testDirectory, 'actual.json'), result, { log: false });
+    const actualFilepath = join(cacheRoot, testDirectory, 'actual.json');
+    await fs.writeJSON(actualFilepath, result, { log: false });
+
+    const expectedPath = join(cacheRoot, testDirectory, 'expected.json');
+    const fullExpected = await fs.readJSON(expectedPath);
 
     // Ignore features (for now?).
     const removeFeatures = d => { delete d.feature; return d; };
     const actual = JSON.stringify(result.map(removeFeatures));
     const expected = JSON.stringify(fullExpected.map(removeFeatures));
 
-    t.equal(actual, expected, `${sname} on ${date}`);
+    const msg = `${sname} on ${date} (actual.json file saved in ${testDirectory})`;
+    t.equal(actual, expected, msg);
   }
   else {
     t.fail(`should have had a result for ${sname} on ${date}`);
@@ -131,9 +127,12 @@ async function runTest(t, testDirectory) {
 }
 
 
+const cachePath = join(process.cwd(), 'tests', 'integration', 'scrapers', 'testcache');
+
 const testDirs = fastGlob.
       sync(join(cachePath, '**'), { onlyDirectories: true }).
-      filter(s => /\d{4}-\d{2}-\d{2}$/.test(s));
+      filter(s => /\d{4}-\d{2}-\d{2}$/.test(s)).
+      map(s => s.replace(`${cachePath}${path.sep}`, ''));
 
 const lock = new Lock(testDirs.length);
 
@@ -142,7 +141,7 @@ test('Parsers', async t => {
   testDirs.forEach(async d => {
     await lock.acquire();
     try {
-      await runTest(t, d);
+      await runTest(t, cachePath, d);
     }
     finally {
       lock.release();
