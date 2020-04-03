416ee1ce2e89bc664846d76dbf43b1efc6aac2cc
Jeff Zohrab <jzohrab@gmail.com>
2020-04-02 21:40:16 -0600

    WIP pull out

--- tests/integration/scrapers/scrapers-all-test.js
+++ tests/integration/scrapers/scrapers-all-test.js
@@ -57,92 +57,93 @@ const testDirs = fastGlob.
 
       // filter(s => /\d{4}-\d{2}-\d{2}$/.test(s)).
       // slice((SLICE_START * 1), (SLICE_START * 1) + 1);
-console.log("RUNNING TESTS FOR: ");
-console.log(testDirs);
-
-// ONLY DOING ONE OF THEM
-
-const scrapersAndDates = testDirs.map(s => scraperNameAndDateFromPath(s));
-// console.log(scrapersAndDates);
+// console.log("RUNNING TESTS FOR: ");
+// console.log(testDirs);
 
 
-const scraperSourcePathRoot = join(__dirname, '..', '..', '..', 'src', 'shared', 'scrapers');
+async function runTest(t, d) {
 
-test('Parsers', async t => {
-  t.plan(testDirs.length);
-  testDirs.forEach(async d => {
+  const pair = scraperNameAndDateFromPath(d);
+  const sname = pair.scraperName;
+  const date = pair.date;
 
-    const pair = scraperNameAndDateFromPath(d);
-    const sname = pair.scraperName;
-    const date = pair.date;
-    const spath = join(scraperSourcePathRoot, sname, 'index.js');
+  const scraperSourcePathRoot = join(__dirname, '..', '..', '..', 'src', 'shared', 'scrapers');
+  const spath = join(scraperSourcePathRoot, sname, 'index.js');
 
-    console.log(`******** TEST ${sname} ON ${date} ********************`);
+  console.log(`******** TEST ${sname} ON ${date} ********************`);
 
-    // Read sample responses for this scraper and pass them to the mock `get` function.
-    const sampleResponses = fastGlob.sync(join(d, '*')).filter(p => !p.includes('expected'));
-    console.log(sampleResponses);
+  /*
+  // Read sample responses for this scraper and pass them to the mock `get` function.
+  const sampleResponses = fastGlob.sync(join(d, '*')).filter(p => !p.includes('expected'));
+  console.log(sampleResponses);
 
-    let getReturns = {}
-    for (const filePath of sampleResponses) {
-      // MAGIC HERE:
-      // The filenames are stored as the sanitized-url values.
-      const sanitizedURL = path.basename(filePath);
-      const content = await fs.readFile(filePath);
-      getReturns[sanitizedURL] = content.toString();
-    }
+  let getReturns = {}
+  for (const filePath of sampleResponses) {
+    // MAGIC HERE:
+    // The filenames are stored as the sanitized-url values.
+    const sanitizedURL = path.basename(filePath);
+    const content = await fs.readFile(filePath);
+    getReturns[sanitizedURL] = content.toString();
+  }
 
-    console.log("Fake returns and data sizes:");
-    console.log("{");
-    Object.keys(getReturns).forEach(k => {
-      console.log("  " + k + ": " + getReturns[k].length);
-    });
-    console.log("}");
-    
-    get.get = async (url, type, date, options) => {
-      console.log("CALLING FOR " + url);
-      const sanurl = sanitize.sanitizeUrl(url);
-      console.log("SANITIZED: " + sanurl);
-      const respFile = join(d, sanurl);
-      console.log("RESPONSE FILE: " + respFile);
-      return await fs.readFile(respFile);
-      /*
+  console.log("Fake returns and data sizes:");
+  console.log("{");
+  Object.keys(getReturns).forEach(k => {
+    console.log("  " + k + ": " + getReturns[k].length);
+  });
+  console.log("}");
+  */
+  
+  get.get = async (url, type, date, options) => {
+    console.log("CALLING FOR " + url);
+    const sanurl = sanitize.sanitizeUrl(url);
+    console.log("SANITIZED: " + sanurl);
+    const respFile = join(d, sanurl);
+    console.log("RESPONSE FILE: " + respFile);
+    return await fs.readFile(respFile);
+    /*
       const ret = getReturns[sanurl];
       console.log("DO WE HAVE RET?" + (ret !== null && ret !== undefined));
       if (ret === null || ret === undefined) {
-        console.log(`${sname}/${date}: missing data ${sanurl}`);
-        console.log(`Have keys: ${Object.keys(getReturns)}`);
+      console.log(`${sname}/${date}: missing data ${sanurl}`);
+      console.log(`Have keys: ${Object.keys(getReturns)}`);
       }
       return ret;
-      */
-    };
+    */
+  };
 
-    const expected = await fs.readJSON(join(d, 'expected.json'));
+  const expected = await fs.readJSON(join(d, 'expected.json'));
     
-    const scraperObj = imports(spath).default;
-    process.env.SCRAPE_DATE = date;
+  const scraperObj = imports(spath).default;
+  process.env.SCRAPE_DATE = date;
 
-    let result = null;
-    try {
-      console.log(`>>>> START SCRAPING FOR ${sname} ON ${date} ********************`);
-      result = await runScraper.runScraper(scraperObj);
-      console.log(`<<<< DONE  SCRAPING FOR ${sname} ON ${date} ********************`);
-    }
-    catch (e) {
-      t.fail(`error scraping: ${e}`);
-    }
+  let result = null;
+  try {
+    console.log(`>>>> START SCRAPING FOR ${sname} ON ${date} ********************`);
+    result = await runScraper.runScraper(scraperObj);
+    console.log(`<<<< DONE  SCRAPING FOR ${sname} ON ${date} ********************`);
+  }
+  catch (e) {
+    t.fail(`error scraping: ${e}`);
+  }
 
-    if (result) {
-      await fs.writeJSON(join(d, 'actual.json'), result);
-      const actual = result.map(stripFeatures);
-      t.equal(JSON.stringify(actual), JSON.stringify(expected.map(stripFeatures)));
-    }
-    else {
-      t.fail(`should have had a result for ${sname} on ${date}`);
-    }
+  delete process.env.SCRAPE_DATE;
 
-    delete process.env.SCRAPE_DATE;
-  });
+  if (result) {
+    await fs.writeJSON(join(d, 'actual.json'), result);
+    const actual = result.map(stripFeatures);
+    t.equal(JSON.stringify(actual), JSON.stringify(expected.map(stripFeatures)));
+  }
+  else {
+    t.fail(`should have had a result for ${sname} on ${date}`);
+  }
+
+}
+
+
+test('Parsers', async t => {
+  t.plan(testDirs.length);
+  testDirs.forEach(async d => { await runTest(t, d); });
 });
 
 // Final cleanup.
