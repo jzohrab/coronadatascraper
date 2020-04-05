cc7b6faabfc0ef8537b2d421189d8a0302a521b4
Jeff Zohrab <jzohrab@gmail.com>
2020-04-05 14:05:05 -0600

    wip script

--- scripts/scraperCodeFixes/cache-migration-hacks.rb
+++ scripts/scraperCodeFixes/cache-migration-hacks.rb
@@ -1,14 +1,24 @@
 # Hack all of the scraper files
 
 
-$locationRE = /(\s*)(city|county|state|country):/
+LOCATION_RE = /(\s*)(city|county|state|country):/
+
+# Some scraper files aren't really scrapers.
+# Rather than try to determine these programmatically,
+# just ignore specific ones.
+IGNORE_FILES = %w(
+AUS/_shared/get-data-with-tested-negative-applied.js
+AUS/_shared/get-key.js
+DEU/_shared.js
+)
+
 
 def validate(scraper_dir, f)
   fpath = File.join(scraper_dir, f)
   src = File.read(fpath)
 
-  if (src !~ $locationRE)
-    raise "No match for #{locationRE} in #{f}"
+  if (src !~ LOCATION_RE)
+    raise "No match for #{LOCATION_RE} in #{f}"
   end
 end
 
@@ -18,8 +28,7 @@ def add_filename_to_scraper_this(scraper_dir, f)
   src = File.read(fpath)
 
   puts f
-  m = src.match($locationRE)
-  puts m.inspect
+  m = src.match(LOCATION_RE)
 
   spaces = m[1].gsub("\n", '')
   loctype = m[2]
@@ -28,7 +37,7 @@ def add_filename_to_scraper_this(scraper_dir, f)
 #{spaces}_filepath: __filename,
 #{spaces}#{loctype}:"
 
-  src = src.sub($locationRE, add_code)
+  src = src.sub(LOCATION_RE, add_code)
   puts src
 end
 
@@ -41,8 +50,15 @@ files = []
 Dir.chdir(scraper_dir) do
   files = Dir.glob(File.join('**', '*.js'))
 end
+# puts "Pre remove count: #{files.count}"
+files -= IGNORE_FILES
+# puts "Post remove count: #{files.count}"
 
 puts "#{files.size} scraper files."
 
 # During dev, just do one file.
-add_filename_to_scraper_this(scraper_dir, files[0])
+# add_filename_to_scraper_this(scraper_dir, files[0])
+
+files.each do |f|
+  validate(scraper_dir, f)
+end
