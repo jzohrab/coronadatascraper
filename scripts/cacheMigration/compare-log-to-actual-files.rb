16c89865f889726118acecde12b77364e12603b8
Jeff Zohrab <jzohrab@gmail.com>
2020-04-05 17:59:58 -0600

    Finish analysis

--- scripts/cacheMigration/compare-log-to-actual-files.rb
+++ scripts/cacheMigration/compare-log-to-actual-files.rb
@@ -26,6 +26,28 @@ j = get_cachecalls_json()
 jout =
   j.select { |h| h['cacheFilePath'] =~ /coronadatascraper-cache/ }.
     map { |h| clean_output(h) }
-         
+files_returned = jout.map { |h| h['cacheFilePath'] }.uniq.sort
 
-puts JSON.pretty_generate(jout)
+# puts JSON.pretty_generate(jout)
+
+cachedir = File.join(__dir__, '..', '..', 'coronadatascraper-cache')
+files = []
+Dir.chdir(cachedir) do
+  files = Dir.glob(File.join('**', '*.*'))
+end
+all_files =
+  files.
+    map { |f| File.join("coronadatascraper-cache", f) }.
+    uniq.
+    sort
+
+cache_hits = files_returned & all_files
+unused_files = all_files - files_returned
+
+puts "The following #{unused_files.size} files were NOT INCLUDED in cacheCalls.txt"
+puts
+puts unused_files
+puts "\n(#{unused_files.size} files)"
+
+# puts '*' * 60
+# puts unused_files.select { |s| s =~ /-25/ }
