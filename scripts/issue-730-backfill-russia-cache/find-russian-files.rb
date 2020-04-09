fe73b6d3022073a09810a20d43c9794015e5525e
Jeff Zohrab <jzohrab@gmail.com>
2020-04-09 11:50:49 -0600

    wip

--- scripts/issue-730-backfill-russia-cache/find-russian-files.rb
+++ scripts/issue-730-backfill-russia-cache/find-russian-files.rb
@@ -1,28 +1,44 @@
 # coding: utf-8
 # Finds all files in the cache containing russian characters.
 
+require 'digest/md5'
 
-
-cachedir = File.join(__dir__, '..', '..', 'coronadatascraper-cache')
+CACHEDIR = File.join(__dir__, '..', '..', 'coronadatascraper-cache')
 files = []
-Dir.chdir(cachedir) do
-  files = Dir.glob(File.join('**', '*.*'))
+russian_files = []
+
+def make_hash(f)
+  {
+    fullpath: File.join(CACHEDIR, f),
+    relpath: f,
+    hexdigest: Digest::MD5.hexdigest(File.read(f))
+  }
 end
-all_files =
-  files.
-    map { |f| File.join("coronadatascraper-cache", f) }.
-    uniq.
-    sort
 
 
-russian_chars = "населением"
-russian_files =
-  files.
-    map { |f| File.join(cachedir, f) }.
-    select { |f| f =~ /json$/ }.
-    select { |f| File.read(f) =~ /[#{russian_chars}]/ }
+Dir.chdir(CACHEDIR) do
+  files = Dir.glob(File.join('**', '*.*'))
+
+  # Get the files and contents
+  russian_chars = "населением"
+  russian_files =
+    files.
+      select { |f| f =~ /json$/ }.
+      select { |f| File.read(f) =~ /[#{russian_chars}]/ }.
+      map { |f| make_hash(f) }
+end
+
+# example = russian_files[0].clone
+# example.delete(:content)
+# puts example
 
 
-puts russian_files
+digests = russian_files.map { |hsh| hsh[:hexdigest] }.sort.uniq
+puts "#{digests.count} digests, #{russian_files.count} files"
 
-# Group by date.
+digests.each do |d|
+  puts '-' * 40
+  puts d
+  puts russian_files.select { |r| r[:hexdigest] == d }
+  puts
+end
