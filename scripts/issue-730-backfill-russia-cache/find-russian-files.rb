33d8fa460211a7015194b83435d3bde295b4d0ba
Jeff Zohrab <jzohrab@gmail.com>
2020-04-09 12:03:19 -0600

    wip

--- scripts/issue-730-backfill-russia-cache/find-russian-files.rb
+++ scripts/issue-730-backfill-russia-cache/find-russian-files.rb
@@ -10,6 +10,7 @@ russian_files = []
 def make_hash(f)
   {
     fullpath: File.join(CACHEDIR, f),
+    date: f.split('/')[0],
     relpath: f,
     hexdigest: Digest::MD5.hexdigest(File.read(f))
   }
@@ -19,26 +20,46 @@ end
 Dir.chdir(CACHEDIR) do
   files = Dir.glob(File.join('**', '*.*'))
 
-  # Get the files and contents
   russian_chars = "населением"
-  russian_files =
+  files =
     files.
       select { |f| f =~ /json$/ }.
       select { |f| File.read(f) =~ /[#{russian_chars}]/ }.
       map { |f| make_hash(f) }
 end
 
-# example = russian_files[0].clone
+# example = files[0].clone
 # example.delete(:content)
 # puts example
 
 
-digests = russian_files.map { |hsh| hsh[:hexdigest] }.sort.uniq
-puts "#{digests.count} digests, #{russian_files.count} files"
+digests = files.map { |hsh| hsh[:hexdigest] }.sort.uniq
+puts "#{digests.count} digests, #{files.count} files"
 
+puts "\n\nFILES GROUPED BY DIGESTS:"
 digests.each do |d|
   puts '-' * 40
   puts d
-  puts russian_files.select { |r| r[:hexdigest] == d }
+  puts files.select { |r| r[:hexdigest] == d }
   puts
+end
+
+puts "\n\nDIGESTS GROUPED BY DATES:"
+date_digests = {}
+files.map { |f| f[:date] }.sort.uniq. each do |dt|
+  date_digests[dt] = files.select { |f| f[:date] == dt }.map { |f| f[:hexdigest] }.sort.uniq
+end
+date_digests.each_key do |dt|
+  puts "#{dt}: #{date_digests[dt]}"
+end
+
+puts "\n\nDATES WHERE EACH DIGEST IS FOUND"
+dg_dates = {}
+files.map { |f| f[:hexdigest] }.sort.uniq. each do |dg|
+  dg_dates[dg] = files.select { |f| f[:hexdigest] == dg }.map { |f| f[:date] }.sort.uniq
+end
+dg_dates.each_key do |dg|
+  dates = dg_dates[dg]
+  raise "Digest #{dg} found in multiple dates #{dates}" if (dates.length != 1)
+  puts "#{dg}: #{dates}"
 end
