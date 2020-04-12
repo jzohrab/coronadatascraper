6a0505985ea483aaaac8b52ad9b0424f4ffbe21a
Jeff Zohrab <jzohrab@gmail.com>
2020-04-11 19:55:42 -0600

    wip

--- src/shared/lib/json-diff.js
+++ src/shared/lib/json-diff.js
@@ -1,3 +1,13 @@
 export default function jsonDiff(left, right) {
-  return [];
+
+  /** Iterates through the left and right, pushes errors (differences)
+   * onto errs. */
+  function _jsonDiffIter(left, right, errs) {
+
+  }
+
+  const errs = [];
+  _jsonDiffIter(left, right, errs);
+
+  return errs;
 }
