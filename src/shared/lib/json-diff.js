fbd742b49d77624ddea83dc38e351edb04ac3ecc
Jeff Zohrab <jzohrab@gmail.com>
2020-04-11 20:07:50 -0600

    wip

--- src/shared/lib/json-diff.js
+++ src/shared/lib/json-diff.js
@@ -2,12 +2,36 @@ export default function jsonDiff(left, right) {
 
   /** Iterates through the left and right, pushes errors (differences)
    * onto errs. */
-  function _jsonDiffIter(left, right, errs) {
+  function _jsonDiffIter(left, right, currPath, errs) {
     errs.push('hi');
+
+    if(left == right) {
+        return;
+    }
+
+    if(isPrimitive(left) && isPrimitive(right)) {
+      if (left != right) {
+        errs.push(`${currPath} value: ${left} != ${right}`);
+        return;
+      }
+    }
+
+    const leftKeys = Object.keys(left).sort().toString();
+    const rightKeys = Object.keys(right).sort().toString();
+    if(leftKeys !== rightKeys) {
+      errs.push(`${currPath} keys: [${leftKeys}] != [${rightKeys}]}`);
+      return;
+    }
+
+    // compare objects with same keys
+    leftKeys.forEach(k => {
+      _jsonDiffIter(left[k], right[k], `${currPath}/`, errs);
+    });
+
   }
 
   const errs = [];
-  _jsonDiffIter(left, right, errs);
+  _jsonDiffIter(left, right, '', errs);
 
   return errs;
 }
