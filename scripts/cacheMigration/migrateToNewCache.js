d5d30565783550eb0ae650c089c7bcd6d48c8b12
Jeff Zohrab <jzohrab@gmail.com>
2020-04-13 16:05:51 -0600

    wip

--- scripts/cacheMigration/migrateToNewCache.js
+++ scripts/cacheMigration/migrateToNewCache.js
@@ -5,7 +5,7 @@
 
 const imports = require('esm')(module);
 const fs = imports('fs');
-const { join } = imports('path');
+const path imports('path');
 
 // Get all the entries in the log_cacheCalls as json.
 const raw = fs.readFileSync('log_cacheCalls.txt', 'utf-8');
@@ -20,3 +20,22 @@ const hits = json.filter(h => {
   return (h['cacheFileExists'] === true) && (h['cacheFilePath'].startsWith('coronadatascraper-cache')); 
 });
 console.log(`Hits: ${hits.length}`);
+
+
+function sourceDirectoryName(origPath) {
+  const re = /coronadatascraper.src.shared.scrapers\//;
+  var ret = origPath
+      .replace(re, '')
+      .toLowerCase()
+      .replace('index.js', '')
+      .replace('.js', '')
+      .replace(/${path.sep}/, '-');
+  return ret;
+}
+
+// New filename format:
+// crawler-cache/us-ca-san-francisco-county/2020-04-12/2020-04-12t00_47_14.145z-default-344b7.html
+
+// For each date, find if any scrapers made multiple calls.  These
+// need to have special keys created.  These will be in a data array,
+// and if they're not, that's an error.
